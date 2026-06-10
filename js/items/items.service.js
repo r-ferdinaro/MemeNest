67fa5cd071e9b5f4bc1c4ca04c1bd5190b6afa00
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-06-11 01:33:07 +0300

    Refactor code & update todos

---
+++ js/items/items.service.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const IMAGE_STORAGE_KEY = 'imageDB'
+const MEME_STORAGE_KEY = 'memeDB'
+
+let gMemes
+let gImgs
+_createImages()
+
+// Get all/some images/memes to gallery based on search filter
+function getItems(page, searchFilter) {
+    const dataSource = (page === 'gallery') ? gImgs : gMemes
+    if (!searchFilter || !searchFilter.trim()) return dataSource
+    
+    const keywordsArr = searchFilter.trim().toLowerCase().split(/\s+/);
+    
+    return dataSource.filter(obj => {
+        if (!obj.keywords) return false;
+        return keywordsArr.every(searchStr => {
+            return obj.keywords.some(keyword =>
+                keyword.toLowerCase().includes(searchStr)
+            )
+        })
+    })
+}
+
+// TODO: use this to get desired image/meme from storage
+function getItemById(type, id) {
+    const dataSource = (type === 'image') ? gImgs : gMemes
+    return dataSource.find(item => id === item.id)
+}
+
+// TODO: implement deleting a photo/meme
+// function deleteItem(type, id) {
+//     const dataSource = (type === 'image') ? gImgs : gMemes
+//     const itemIdx = dataSource.findIndex(item => itemId === item.id)
+
+//     dataSource.splice(itemIdx, 1)
+//     _saveImgsToStorage()
+// }
+
+// TODO: save meme from editor page
+function saveMeme() {
+    (gMeme.id) ? _updateMeme() : _saveMeme()
+}
+
+// // TODO: support uploads in a future PR.
+// function uploadImg(url, keywords) {
+//     // TODO: add a property to gImgs to differ between local/uploaded images. 
+//     // If local image - grab from files | if uploaded image - save to localStorage as dataURL.
+//     // Will require getGalleryContent to convert the dataURL to an image and insert into img element properly. 
+//     const img = _createImg(url, keywords)
+    
+//     gImgs.unshift(img)
+//     _saveImgsToStorage()
+//     return pic
+// }
+
+// Private functions
+
+// Create image object
+function _createImg(url, keywords = ['user']) {
+    return {
+        id: makeId(),
+        url,
+        keywords
+    }
+}
+
+// Save new meme to Storage
+function _saveNewMeme() {
+    const meme = {
+        id: makeId(),
+        ...gMeme
+    }
+    
+    gMemes.unshift(meme)
+    _saveImgsToStorage()
+}
+
+// Save an existing meme
+function _updateMeme() {
+    const memeIdx = gMemes.findIndex(meme => meme.id === gMeme.id)
+    
+    gMemes.splice(memeIdx, 1, gMeme)
+}
+
+// Load images from localStorage or load sample ones
+function _createImages() {
+    let images = loadFromStorage(IMAGE_STORAGE_KEY)
+
+    if (!images || !images.length) {
+        const sampleImages = [
+            {
+                url: "images/1.jpg",
+                keywords:['trump', 'funny']
+            },
+            {
+                url: "images/2.jpg",
+                keywords:['puppy', 'cute']
+            }
+        ]
+        images = []
+        
+        sampleImages.forEach(img => images.push(_createImg(img.url, img.keywords)))
+    }
+
+    gImgs = images
+    _saveImgsToStorage()
+}
+
+// Save updated images to localStorage
+function _saveImgsToStorage() {
+    saveToStorage(IMAGE_STORAGE_KEY, gImgs)
+}
+
+// Save created images to localStorage
+function _saveMemesToStorage() {
+    saveToStorage(MEME_STORAGE_KEY, gMemes)
+}
--- js/page/page.controller.js
+++ js/page/page.controller.js
@@ -2,123 +2,54 @@
 
 const gQueryParams = {
     page: '',
-    // TODO: check what is best implementation for filterBy. should it be set as one full string or array.... what's the best approach
-    filterBy: '',
-    imageId: '',
-    memeId: ''
+    filterBy: ''
 }
-const PAGES = ['gallery', 'editor', 'memes', 'about']
 
 // Page rendering functions
+
 function onInit() {
     readQueryParams()
     renderPageContent()
     renderQueryParamsFilters()
 }
 
-// TODO: might want to just trigger relevant functions - lets see
-// Render desired content to the main section (i think) container
-// function might be in services 
 function renderPageContent() {
     const { page } = gQueryParams
-    const elContainer = document.querySelector('.main-content-container')
+    // const elContainer = document.querySelector('.main-content-container')
     
-    let strHtml
     if (['gallery', 'memes'].includes(page)) {
         const galleryContent = getGalleryContent()
-        strHtml = `
-            <section class="gallery">
-                <div class="filter-section">
-                    <input name="photos-search" class="search-bar" type="text" oninput="OnSetSearchFilter(this)">
-                    <button class="btn search-bar-reset" onclick="onClearSearchFilter(this)">Clear filters</button>
-                    <div class="tags-container">
-                        <a href="#" value="happy" onclick="onTagFilter(this)">happy</a>
-                        <a href="#" value="sad" onclick="onTagFilter(this)">sad</a>
-                        <a href="#" value="trump" onclick="onTagFilter(this)">trump</a>
-                        <a href="#" value="putin" onclick="onTagFilter(this)">putin</a>
-                    </div>
-                    <button class="expand">More</button>
-                    <button class="filter-btn" onclick="onToggleMenu(this)" type="button"><i class="fa-solid fa-sliders"></i></button>
-                </div>
-                <div class="gallery-content"> ${galleryContent}</div>
-            </section>
-        `
+        
+        document.querySelector('.gallery-content').innerHTML = galleryContent
+        document.querySelector('.gallery').classList.remove('hide')
+        document.querySelector('.editor').classList.add('hide')
+        document.querySelector('.about').classList.add('hide')
     } else if (page === 'editor') {
-        strHtml = `
-            <section class="editor">
-                <div class="canvas-area">
-                    <div class="canvas-container">
-                        <canvas height="300"></canvas>
-                    </div>
-                </div>
-                <div class="canvas-tools"></div>
-            </section>
-        `
-    } else if (page === 'about') {
-        strHtml = ``
+        document.querySelector('.editor').classList.remove('hide')
+        document.querySelector('.gallery').classList.add('hide')
+        document.querySelector('.about').classList.add('hide')
+        onEditorInit()
+    } else {
+        document.querySelector('.about').classList.remove('hide')
+        document.querySelector('.gallery').classList.add('hide')
+        document.querySelector('.editor').classList.add('hide')
     }
-
-    // implementation of the innerHTML that will be inserted to the container
-    // elContainer.innerHTML = strHtml.join('')
-    elContainer.innerHTML = strHtml
 }
 
-// query params functions
-// Read query params from search bar
-function readQueryParams() {
-    const readQueryParams = new URLSearchParams(window.location.search)
-
-    gQueryParams.page = readQueryParams.get('page') || 'gallery'
-    
-    gQueryParams.filterBy = readQueryParams.get('filterBy') || ''
-    gQueryParams.imageId = readQueryParams.get('imageId') || ''
-    gQueryParams.memeId = readQueryParams.get('memeId') || ''
-}
-
-// Render queryParams values to filter section
-function renderQueryParamsFilters() {
+// Get images/memes from storage to img tags
+// TODO: support memes
+function getGalleryContent() {
     const { page, filterBy } = gQueryParams
-    if (!['gallery', 'memes'].includes(page) || !filterBy) return
+    const contentArray = getItems(page, filterBy)
+    let strHtml = ``
     
-    document.querySelector('.search-bar').value = filterBy
-    // TODO: if search content words are part of existing tags, show them first in the list of available tags and change their style so it's obvious
-}
-
-// TODO: should be triggered upon changing the page 
-// TODO: should be triggered upon changing the search filter values or selecting a specific tag
-// Set current query params to the search bar
-function setQueryParams() {
-    const queryParams = new URLSearchParams()
-    const { protocol, host, pathname } = window.location;
-    const { page = 'gallery', filterBy, imageId, memeId } = gQueryParams;
-
-    const targetPage = (page === 'editor' && !memeId && !imageId) ? 'gallery' : page
-    queryParams.set('page', targetPage)
-
-    switch (targetPage) {
-        case 'gallery':
-        case 'memes':
-            if (filterBy) queryParams.set('filterBy', filterBy);
-            break;
-        case 'editor':
-            if (memeId) queryParams.set('memeId', memeId);
-            else if (imageId) queryParams.set('imageId', imageId);
-            break;
+    if (contentArray) {
+        contentArray.forEach( img => strHtml += `<img data-idx="${img.id}" src="${img.url}">`)
     }
-
-    const newUrl = `${protocol}//${host}${pathname}?${queryParams.toString()}`
-    window.history.pushState({path: newUrl}, '', newUrl)
+    return strHtml
 }
 
-function onToggleMenu(el) {
-    const menuTypeClass = el.classList.contains('filter-btn') ? 'filter' : 'nav'
-    document.body.classList.add('menu-open', `${menuTypeClass}`);
-}
-
-function onCloseMenus() {
-    document.body.classList.remove('menu-open', 'filter', 'nav');
-}
-
+// change page based on nav click
 function onPageChange(ev) {
     ev.preventDefault()
 
@@ -127,11 +58,76 @@ function onPageChange(ev) {
     renderPageContent()
 }
 
+// TODO: trigger search based on the target value
+// TODO: trigger setQueryParams
 function OnSetSearchFilter(el) {
 }
 
+// TODO: if tag doesn't exist in search value - add to the end of the search value
+// TODO: if tag exists in search value - remove from search value
+// TODO: toggle class that will help understand wether tag is selected or not
+// TODO: trigger search based on the searchbar target value, 
+// TODO: trigger setQueryParams
 function onTagFilter(el) {
 }
 
+// TODO: clear value from search bar
+// TODO: clear special class from all tags
+// TODO: trigger search based on the searchbar target value, 
+// TODO: trigger setQueryParams
 function onClearSearchFilter() {
 }
+
+
+// query params functions
+
+// Read query params from search bar
+function readQueryParams() {
+    const queryParams = new URLSearchParams(window.location.search)
+
+    gQueryParams.page = queryParams.get('page') || 'gallery'
+    gQueryParams.filterBy = queryParams.get('filterBy') || ''
+}
+
+// Render queryParams values to filter section
+function renderQueryParamsFilters() {
+    const { page, filterBy } = gQueryParams
+    
+    // Early return if irrelevant page or no filter value
+    if (['editor', 'about'].includes(page) || !filterBy) return
+    
+    document.querySelector('.search-bar').value = filterBy
+    // TODO: if search content words are part of existing tags, change the tags order so they are first to show, and change their style so it's obvious they are selected
+}
+
+// TODO: should be triggered upon changing the page 
+// TODO: should be triggered upon changing the search filter values or selecting a specific tag
+// TODO: should be triggered when user clicks on image/meme to open editor
+// Set current query params to the search bar
+function setQueryParams() {
+    const queryParams = new URLSearchParams()
+    const { protocol, host, pathname } = window.location
+    const { page = 'gallery', filterBy } = gQueryParams
+    
+    queryParams.set('page', page)
+    if (['gallery', 'editor'].includes(page)) {
+        if (filterBy) queryParams.set('filterBy', filterBy)
+    }
+    
+    const newUrl = `${protocol}//${host}${pathname}?${queryParams.toString()}`
+    window.history.pushState({path: newUrl}, '', newUrl)
+}
+
+// Mobile only - Menu visibility functions
+
+// Show/hide nav/filter menu
+function onToggleMenu(el) {
+    const menuTypeClass = el.classList.contains('filter-btn') ? 'filter' : 'nav'
+    document.body.classList.add('menu-open', `${menuTypeClass}`)
+}
+
+// Close nav/filter menu
+function onCloseMenu() {
+    document.body.classList.remove('menu-open', 'filter', 'nav')
+}
+
